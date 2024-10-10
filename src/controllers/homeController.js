88317dd36931debc668c286f5170048eb7fe20ad
QuanNVph3649 <quanv8786@gmail.com>
2024-10-10 11:23:17 +0700

    sửa xóa user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -20,10 +20,39 @@ const postCreateUser = async (req, res)=> {
     let [results, fields] = await connection.query(
         `INSERT INTO users (email, name) VALUES (?,?)`, [email, name],
     );
+    res.send("thêm thành công");
+}
+const getUpdateUser = async (req, res) => {
+    const userId = req.params.id
+    let [results, fields] = await connection.query('SELECT * FROM users WHERE id = ?', [userId]);
+    let user = results && results.length > 0 ? results[0] : {}; 
+    return res.render('update.ejs',{user : user});
+   
+}
+const postUpdateUser = async (req, res) => {
+    let email = req.body.email;
+    let name = req.body.name;
+    let id = req.body.id;
+    
+    let [results, fields] = await connection.query(
+        `UPDATE users SET email=?, name=? WHERE id=? `, [email, name, id]
+    );
+     res.redirect('/'); 
+}
+
+const deleteUser = async (req, res) => {
+    let id = req.params.id;
+    let [results, fields] = await connection.query(
+        `DELETE FROM users WHERE id=?`,[id]
+    );
+     res.redirect('/'); 
 }
 
 module.exports = {
     getHomepage,
     postCreateUser,
-    getCreateUser
+    getCreateUser,
+    getUpdateUser,
+    postUpdateUser,
+    deleteUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const {getHomepage,getCreateUser,postCreateUser} = require('../controllers/homeController');
+const {getHomepage,getCreateUser,postCreateUser,getUpdateUser,postUpdateUser, deleteUser} = require('../controllers/homeController');
 const router = express.Router();
 
 router.get('/', getHomepage)  
 router.get('/create', getCreateUser)  
 router.post('/create-user', postCreateUser) 
+
+router.get('/update/:id', getUpdateUser)  
+router.post('/update-user/:id', postUpdateUser) 
+
+router.get('/delete-user/:id', deleteUser) 
+
+
 
 
 module.exports = router; // export default 
